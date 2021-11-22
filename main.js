6b03d70d11035d8e4f14c8b51ba8bdd18d62a431
ManuelEscribano <manuel.escribano@gmx.de>
2021-11-22 03:56:19 -0300

    Add playRound function

--- main.js
+++ main.js
@@ -7,15 +7,12 @@ let computerSelection = function () {
   switch (randomNum) {
     case 1:
       selectedC = "rock";
-      console.log("rock");
       return selectedC;
     case 2:
       selectedC = "paper";
-      console.log("paper");
       return selectedC;
     default:
       selectedC = "scissor";
-      console.log("scissor");
       return selectedC;
   }
 };
@@ -35,3 +32,33 @@ let playerSelection = function () {
   );
   return selectedP;
 };
+
+// Play round.
+let playRound = function () {
+  playerSelection();
+  computerSelection();
+  if (selectedP === selectedC) {
+    console.log(`It's a tie`);
+  } else if (selectedP === "rock") {
+    if (selectedC === "paper") {
+      console.log(`The computer has won!`);
+    } else {
+      console.log("You have won!");
+    }
+  } else if (selectedP === "paper") {
+    if (selectedC === "scissor") {
+      console.log(`The computer has won!`);
+    } else {
+      console.log(`You have won!`);
+    }
+  } else if (selectedP === "scissor") {
+    if (selectedC === "rock") {
+      console.log(`The computer has won!`);
+    } else {
+      console.log(`You have won!`);
+    }
+  }
+  console.log(`Computer has picked ${selectedC}, and player ${selectedP}`);
+};
+
+playRound();
