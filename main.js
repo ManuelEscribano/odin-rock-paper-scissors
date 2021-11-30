19d92d4feb804827386231718b093af1039eef70
ManuelEscribano <manuel.escribano@gmx.de>
2021-11-30 20:13:53 -0300

    Add functionality related to modal

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
 // Variables
 let playerChoice, computerChoice;
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0,
+  computerScore = 0,
+  roundNumber = 0;
+// const userName = prompt(`Please enter your name:
+// \n`);
 
-let roundNumber = 0;
-const userName = prompt(`Please enter your name:
-\n`);
+// Selectors
 const rpsOptions = Array.from(
   document.querySelector(".human-decision").children
 );
 let computerImg = document.querySelector("#computer-selection");
 let roundElement = document.querySelector(".round-info");
 let scoreElement = document.querySelector(".score");
+let modalWindow = document.querySelector(".modal");
+let closeBtn = document.querySelector("#closeBtn");
+let playAgainBtn = document.querySelector(".restart");
+let restartBtn;
 
+// | UI
 // update userName
-userName
-  ? (document.getElementById("player-name").textContent = userName)
-  : null;
+// userName
+//   ? (document.getElementById("player-name").textContent = userName)
+//   : null;
 
+// Display modal window.
+let displayModal = function () {
+  let modalText = document.querySelector("h2");
+  modalWindow.style.display = "flex";
+  if (playerScore > computerScore) {
+    modalText.textContent = "You have won the game!";
+  } else if (playerScore < computerScore) {
+    modalText.textContent = "You have lost the game..";
+  } else {
+    modalText.textContent = "It's a tie!";
+  }
+};
+
+// Restart game
+playAgainBtn.addEventListener("click", function () {
+  (playerScore = 0), (computerScore = 0), (roundNumber = 0);
+  modalWindow.style.display = "none";
+  updateScoreAndRound();
+});
+
+// Close modal window
+closeBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  modalWindow.style.display = "none";
+});
+
+// | Game logic.
 // Update score
-let updateScoreAndRound = function () {
+function updateScoreAndRound() {
   roundElement.textContent = `Round: ${roundNumber}`;
   scoreElement.textContent = `Game Score: ${playerScore} - ${computerScore}`;
-};
+}
 
 // Computer selection.
 let computerSelection = function () {
@@ -47,7 +80,6 @@ rpsOptions.forEach((btn) => {
     if (playerChoice === computerChoice) {
       roundNumber++;
       updateScoreAndRound();
-      console.log("Its a tie!");
     } else if (playerChoice === "rock") {
       if (computerChoice === "paper") {
         computerScore++, roundNumber++;
@@ -73,5 +105,6 @@ rpsOptions.forEach((btn) => {
         updateScoreAndRound();
       }
     }
+    if (roundNumber === 5) displayModal();
   });
 });
